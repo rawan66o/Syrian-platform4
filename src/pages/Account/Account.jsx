4b824660b2e9eeae079cfce6e9109d95c0640bf8
Louiy Koja <louiykoja38@gmail.com>
2026-06-21 22:41:06 +0300

    refactor: update account layout to include header and footer

--- src/pages/Account/Account.jsx
+++ src/pages/Account/Account.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import styles from "./Account.module.css";
 import SideNav from "../../components/Account/SideNav";
 import { Outlet } from "react-router-dom";
+import Header from "../../components/Headers/Header";
+import Footer from "../../components/Footer/Footer";
 
 const Account = () => {
   return (
-    <div className={styles.Account_container} dir="rtl">
-      <SideNav />
-      <Outlet />
-    </div>
+    <>
+      <Header />
+      <div className={styles.Account_container} dir="rtl">
+        <SideNav />
+        <Outlet />
+      </div>
+      <Footer />
+    </>
   );
 };
 
